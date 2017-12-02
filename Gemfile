01b340015b2301389014cee62b8e3ff53037c9eb
arthur.pardieu <arthur.pardieu@gmail.com>
2017-12-02 06:15:25 -0500

    Add test - cassette

--- Gemfile
+++ Gemfile
@@ -41,3 +41,4 @@ gem 'spring',        group: :development
 
 gem 'nominatim', '~> 0.0.6'
 gem 'forecast_io','~> 2.0.1'
+gem 'vcr', '~> 3.0', '>= 3.0.3', group: [:development, :test]
--- test/controllers/towns_controller_test.rb
+++ test/controllers/towns_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class TownsControllerTest < ActionController::TestCase
   setup do
     @town = towns(:one)
+    @inexisting = towns(:inexisting)
   end
 
   test "should get index" do
@@ -17,16 +18,30 @@ class TownsControllerTest < ActionController::TestCase
   end
 
   test "should create town" do
+    VCR.use_cassette("town ok") do
     assert_difference('Town.count') do
-      post :create, town: { lat: @town.lat, lon: @town.lon, name: @town.name }
+      post :create, town: { name: @town.name }
     end
+  end
 
     assert_redirected_to town_path(assigns(:town))
   end
+  
+  test "should fail if town does not exist" do
+    VCR.use_cassette("town no exist ok") do
+    assert_no_difference('Town.count') do
+      post :create, town: { name: @inexisting.name }
+    end
+  end
+
+    assert_response :success
+  end
 
   test "should show town" do
+    VCR.use_cassette("town show ok") do
     get :show, id: @town
     assert_response :success
+    end
   end
 
   test "should get edit" do
@@ -35,15 +50,17 @@ class TownsControllerTest < ActionController::TestCase
   end
 
   test "should update town" do
+    VCR.use_cassette("town update ok") do
     patch :update, id: @town, town: { lat: @town.lat, lon: @town.lon, name: @town.name }
     assert_redirected_to town_path(assigns(:town))
+    end
   end
 
   test "should destroy town" do
     assert_difference('Town.count', -1) do
       delete :destroy, id: @town
     end
-
+    
     assert_redirected_to towns_path
   end
 end
